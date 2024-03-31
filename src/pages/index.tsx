a973bdcad699b19dc4e05a42f54abdb166d99650
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2024-03-30 21:22:26 -0300

    feat: Novo texto central

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import ContentMiddle from "@/components/ContentMiddle/ContentMiddle";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <ContentMiddle/>
+    </>
   );
 }
